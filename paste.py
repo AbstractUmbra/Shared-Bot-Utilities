979a43f8a3a97c298b778e1bb62b4675a391afdf
Alex Nørgaard <umbra@abstractumbra.dev>
2024-02-01 20:16:04 +0000

    add paste util

---
+++ paste.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import datetime
+
+    from aiohttp import ClientSession
+
+
+async def create_paste(
+    *,
+    content: str,
+    password: str | None = None,
+    expiry: datetime.datetime | None = None,
+    language: str = "py",
+    session: ClientSession,
+) -> str:
+    async with session.post(
+        "https://paste.abstractumbra.dev/data/post", body=content, headers={"Content-Type": f"text/{language}"}
+    ) as resp:
+        data = await resp.json()
+        return data["key"]
