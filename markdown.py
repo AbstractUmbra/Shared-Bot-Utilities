7273daf2f524a23bda00aad504503bc953ddaf8c
Alex Nørgaard <umbra@abstractumbra.dev>
2024-11-22 15:03:02 +0000

    change markdown util to builder pattern

--- markdown.py
+++ markdown.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from copy import copy
 from functools import wraps
-from typing import TYPE_CHECKING, Concatenate, ParamSpec, TypeVar
+from typing import TYPE_CHECKING, Concatenate, ParamSpec, Self, TypeVar
 
 from yarl import URL
 
@@ -20,11 +20,13 @@ def clamp(value: int, /, max_: int, min_: int) -> int:
     return min(max(min_, value), max_)
 
 
-def after_markdown(func: Callable[Concatenate[M, P], None]) -> Callable[Concatenate[M, P], None]:
+def after_markdown(func: Callable[Concatenate[M, P], M]) -> Callable[Concatenate[M, P], M]:
     @wraps(func)
-    def wrapper(item: M, *args: P.args, **kwargs: P.kwargs) -> None:
+    def wrapper(item: M, *args: P.args, **kwargs: P.kwargs) -> M:
         func(item, *args, **kwargs)
         item._inner += "\n"
+
+        return item
 
     return wrapper
 
@@ -47,36 +49,48 @@ class MarkdownBuilder:
         return c
 
     @after_markdown
-    def add_header(self, *, text: str, depth: int = 1) -> None:
+    def add_header(self, *, text: str, depth: int = 1) -> Self:
         depth = clamp(depth, 5, 1)
         self._inner += "#" * depth
         self._inner += " " + text
 
+        return self
+
     @after_markdown
-    def add_subtitle(self, text: str, /) -> None:
+    def add_subtitle(self, text: str, /) -> Self:
         lines = self._inner.split("\n")
         lines.append(f"-# {text}")
         self._inner = "\n".join(lines)
 
-    @after_markdown
-    def add_link(self, *, url: StrOrUrl, text: str) -> None:
-        self._inner += f"[{text}]({url})"
+        return self
 
     @after_markdown
-    def add_bulletpoints(self, *, texts: list[str]) -> None:
+    def add_link(self, *, url: StrOrUrl, text: str) -> Self:
+        self._inner += f"[{text}]({url})"
+
+        return self
+
+    @after_markdown
+    def add_bulletpoints(self, *, texts: list[str]) -> Self:
         builder = ""
         for item in texts:
             builder += f" - {item}\n"
 
         self._inner += builder
 
-    @after_markdown
-    def add_text(self, *, text: str) -> None:
-        self._inner += text
+        return self
 
     @after_markdown
-    def add_newline(self, *, amount: int = 1) -> None:
+    def add_text(self, *, text: str) -> Self:
+        self._inner += text
+
+        return self
+
+    @after_markdown
+    def add_newline(self, *, amount: int = 1) -> Self:
         self._inner += "\n" * amount
+
+        return self
 
     def clear(self) -> None:
         self._inner = ""
