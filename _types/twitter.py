cb1df66c5ab008c314e92b34126759e798eab557
Alex Nørgaard <umbra@abstractumbra.dev>
2026-05-28 16:01:21 +0100

    update twitter types

--- _types/twitter.py
+++ _types/twitter.py
@@ -1,6 +1,9 @@
-from typing import Any, Literal, NotRequired, TypedDict
+from __future__ import annotations
+
+from typing import Any, Literal, NotRequired, Required, TypedDict
 
 MediaType = Literal["media", "video"]
+type TwitterStatusType = TweetDetailsResponse | DeadTweetReponse | None
 
 
 class FacetResponse(TypedDict):
@@ -17,62 +20,162 @@ class WebsiteResponse(TypedDict):
     display_url: str
 
 
+class BirthdayResponse(TypedDict):
+    day: int
+    month: int
+    year: int
+
+
+class VerificationResponse(TypedDict):
+    verified: bool
+    type: Literal["organization", "government", "individual"] | None
+    verified_at: NotRequired[str | None]
+    identity_verified: NotRequired[bool]
+    verified_by: NotRequired[str]
+
+
+class AccountUsernameChangesResponse(TypedDict):
+    count: int
+    last_changed_at: str | None
+
+
+class AccountAboutResponse(TypedDict, total=False):
+    based_in: str | None
+    location_accurate: bool
+    created_country_accurate: bool | None
+    source: str | None
+    username_changes: AccountUsernameChangesResponse
+
+
 class AuthorResponse(TypedDict):
+    type: Literal["profile"]
     id: str
     name: str
     screen_name: str
-    avatar_url: str
-    banner_url: str
+    avatar_url: str | None
+    banner_url: str | None
     description: str
+    raw_description: TweetRawTextResponse
     location: str
     url: str
+    protected: bool
     followers: int
     following: int
-    joined: str  # datetime
+    statuses: int
+    media_count: int
     likes: int
+    joined: str  # datetime
     website: WebsiteResponse
-    tweets: int
-    avatar_colour: str | None
+    birthday: NotRequired[BirthdayResponse]
+    verification: NotRequired[VerificationResponse]
+    about_account: NotRequired[AccountAboutResponse]
+    profile_embed: NotRequired[bool]
 
 
-class _InnerMediaVariantResponse(TypedDict):
-    content_type: str
+class ExternalMediaResponse(TypedDict):
+    type: Literal["video"]
     url: str
-    bitrate: NotRequired[int]  # only for video
+    thumbnail_url: NotRequired[str]
+    height: NotRequired[int]
+    width: NotRequired[int]
 
 
-class _InnerMediaResponse(TypedDict):
+class PhotoMediaResponse(TypedDict):
+    id: NotRequired[str]
+    format: NotRequired[str]
+    type: Literal["photo", "gif"]
     url: str
-    thumbnail_url: str
     width: int
     height: int
-    format: str
-    type: MediaType
-    variants: dict[int, _InnerMediaVariantResponse]
+    transcode_url: NotRequired[str | None]
+    alt_text: NotRequired[str]
 
 
-class _InnerVideoMediaResponse(_InnerMediaResponse):
-    duration: float
+class VideoMediaFormat(TypedDict, total=False):
+    container: Literal["mp4", "webm", "m3u8"]
+    codec: Literal["h264", "hevc", "vp9", "av1"]
+    bitrate: int
+    url: Required[str]
+    size: int
+    height: int
+    width: int
+
+
+class VideoMediaResponse(TypedDict):
+    id: NotRequired[str]
+    format: NotRequired[str]
+    type: Literal["video"]
+    url: str
+    width: int
+    height: int
+    thumbnail_url: NotRequired[str | None]
+    transcode_url: NotRequired[str | None]
+    duration: int
+    filesize: NotRequired[int]
+    formats: list[VideoMediaFormat]
+    publisher: AuthorResponse | None
+
+
+class MosaicMediaFormat(TypedDict):
+    webp: str
+    jpeg: str
+
+
+class MosaicMediaResponse(TypedDict):
+    id: NotRequired[str]
+    format: NotRequired[str]
+    type: Literal["mosaic_photo"]
+    url: str
+    width: int
+    height: int
+    formats: MosaicMediaFormat
+
+
+class BroadcastMediaResponse(TypedDict):
+    url: str
+    ## TODO
 
 
 class MediaResponse(TypedDict):
-    all: dict[int, _InnerMediaResponse] | None
-    videos: dict[int, _InnerVideoMediaResponse] | None
-    images: dict[int, _InnerMediaResponse] | None
+    external: ExternalMediaResponse
+    photos: list[PhotoMediaResponse]
+    videos: list[VideoMediaResponse]
+    all: ExternalMediaResponse | list[PhotoMediaResponse] | list[VideoMediaResponse]
+    mosaic: MosaicMediaResponse
+    broadcast: BroadcastMediaResponse
 
 
 class TweetRawTextResponse(TypedDict):
     text: str
+    display_text_range: list[int]
     facets: dict[int, FacetResponse]
 
 
+class _TwitterPollChoices(TypedDict):
+    label: str
+    count: int
+    percentage: int
+
+
+class TwitterPollDetailsResponse(TypedDict):
+    choices: list[_TwitterPollChoices]
+    total_votes: int
+    ends_at: str
+    time_left_en: str
+
+
 class TweetDetailsResponse(TypedDict):
+    type: Literal["status"]
     url: str
     id: str
     text: str | None
+    reposts: int
+    quotes: int
+    replies: int
+    quote: NotRequired[TwitterStatusType]
+    poll: NotRequired[TwitterPollDetailsResponse]
     raw_text: TweetRawTextResponse
     author: AuthorResponse
-    replies: int
     retweets: int
     likes: int
     created_at: str  # datetime
@@ -84,14 +187,27 @@ class TweetDetailsResponse(TypedDict):
     lang: str
     replying_to: str | None
     replying_to_status: str | None
-    media: NotRequired[MediaResponse]
+    media: MediaResponse
     source: str
     twitter_card: str
     color: str | None
     provider: str
 
 
+class DeadTweetReponse(TypedDict):
+    type: Literal["tombstone"]
+    provider: Literal["twitter"]
+    reason: Literal["deleted", "unavailable", "suspended", "private", "blocked"]
+    message: str
+    id: NotRequred[str]
+    url: NotRequired[str]
+    author: NotRequired[AuthorResponse]
+    at_uri: NotRequired[str]
+    cid: NotRequired[str]
+
+
 class FXTwitterResponse(TypedDict):
     code: int
-    message: str
-    tweet: TweetDetailsResponse
+    status: TweetDetailsResponse | DeadTweetReponse | None
+    thread: list[ThreadDetailsResponse | DeadTweetReponse] | None
+    author: AuthorDetailsResponse
