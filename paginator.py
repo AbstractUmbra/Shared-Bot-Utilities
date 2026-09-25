e5021dac6ccf119c3b7fadaa43167dddb2342f9d
Alex Nørgaard <umbra@abstractumbra.dev>
2026-09-25 18:13:07 +0100

    remove non-"shared" util

--- paginator.py
+++ paginator.py
@@ -9,7 +9,6 @@ This file was sourced from [RoboDanny](https://github.com/Rapptz/RoboDanny).
 from __future__ import annotations
 
 import logging
-from textwrap import shorten
 from typing import TYPE_CHECKING, Any, TypeVar, overload
 
 import discord
@@ -20,16 +19,8 @@ from .ui import BaseView
 
 LOGGER = logging.getLogger(__name__)
 
-try:
-    import hondana  # pyright: ignore[reportMissingImports]  # may not always exist
-except ModuleNotFoundError:
-    HAS_HONDANA = False
-else:
-    HAS_HONDANA = True
-
 if TYPE_CHECKING:
     from collections.abc import Sequence
-    from typing import Self
 
     from utilities.context import Context, Interaction
 
@@ -335,72 +326,6 @@ class SimpleListSource[T](menus.ListPageSource):
     @staticmethod
     async def format_page(_: menus.Menu, entries: T | list[T]) -> T | list[T]:
         return entries
-
-
-if HAS_HONDANA:
-
-    class MangaDexEmbed(discord.Embed):
-        @classmethod
-        async def from_chapter(cls: type[Self], chapter: hondana.Chapter, *, nsfw_allowed: bool = False) -> Self:
-            parent = chapter.manga
-            assert parent is not None
-
-            parent_title = parent.title
-            if chapter.title:
-                parent_title += f" - {chapter.title}"
-            if chapter.chapter:
-                parent_title += f" [Chapter {chapter.chapter}]"
-
-            if parent.cover_url() is None:
-                await parent.get_cover()
-
-            self = cls(title=parent_title, colour=discord.Colour.red(), url=chapter.url)
-            self.set_footer(text=chapter.id)
-            self.timestamp = chapter.created_at
-            self.add_field(name="Manga link is:", value=f"[here!]({parent.url})", inline=False)
-            self.add_field(name="Number of pages:", value=chapter.pages, inline=False)
-            if chapter.scanlator_groups:
-                self.add_field(
-                    name="Scanlator groups:",
-                    value="\n".join([s.name for s in chapter.scanlator_groups]),
-                    inline=False,
-                )
-            if chapter.uploader:
-                self.add_field(name="Uploader:", value=chapter.uploader.username, inline=False)
-
-            if parent.content_rating is hondana.ContentRating.safe or (nsfw_allowed is True):
-                self.set_thumbnail(url=parent.cover_url())
-
-            return self
-
-        @classmethod
-        async def from_manga(cls: type[Self], manga: hondana.Manga, *, nsfw_allowed: bool = False) -> Self:
-            self = cls(title=manga.title, colour=discord.Colour.blue(), url=manga.url)
-            if manga.description:
-                self.description = shorten(manga.description, width=2000)
-            if manga.tags:
-                self.add_field(name="Tags:", value=", ".join([tag.name for tag in manga.tags]), inline=False)
-            if manga.publication_demographic:
-                self.add_field(name="Publication Demographic:", value=manga.publication_demographic.value.title())
-            if manga.content_rating:
-                self.add_field(name="Content Rating:", value=manga.content_rating.value.title(), inline=False)
-            if manga.artists:
-                self.add_field(name="Attributed Artists:", value=", ".join([artist.name for artist in manga.artists]))
-            if manga.authors:
-                self.add_field(name="Attributed Authors:", value=", ".join([artist.name for artist in manga.authors]))
-            if manga.status:
-                self.add_field(name="Publication status:", value=manga.status.value.title(), inline=False)
-                if manga.status is hondana.MangaStatus.completed:
-                    self.add_field(name="Last Volume:", value=manga.last_volume)
-                    self.add_field(name="Last Chapter:", value=manga.last_chapter)
-            self.set_footer(text=manga.id)
-
-            if manga.content_rating is hondana.ContentRating.safe or (nsfw_allowed is True):
-                cover = manga.cover_url() or await manga.get_cover()
-                if cover:
-                    self.set_image(url=manga.cover_url())
-
-            return self
 
 
 class PaginationEmojis(discord.Enum):
